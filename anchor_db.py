1d278407ccfd90fbcecf0a67013f3d9878e9d800
blog <3113999385@qq.com>
2026-06-08 16:12:28 +0800

    perf(db): 启用 WAL 模式以提升数据库并发性能

--- anchor_db.py
+++ anchor_db.py
@@ -22,6 +22,7 @@ class AnchorDB:
     def _conn(self):
         conn = sqlite3.connect(self.db_path)
         conn.row_factory = sqlite3.Row
+        conn.execute("PRAGMA journal_mode = WAL")
         conn.execute("PRAGMA foreign_keys = ON")
         return conn
 
